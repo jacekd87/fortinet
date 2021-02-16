8cb6e919ea03320d293ecf4f84c0eb4fb55394ab
jacekd87 <jacenty87@gmail.com>
2021-02-16 20:21:11 +0100

    Create ssl_cert_check-vhost.sh

---
+++ ssl_cert_check-vhost.sh
@@ -0,0 +1,46 @@
+
+DEBUG=0
+if [ $DEBUG -gt 0 ]
+then
+    exec 2>>/tmp/my.log
+    set -x
+fi
+
+f=$1
+host=$2
+port=$3
+: ${port:=443}
+
+case $f in
+-d)
+end_date=`openssl s_client -servername $host -connect $host:$port -showcerts -prexit </dev/null 2>/dev/null |
+          sed -n '/BEGIN CERTIFICATE/,/END CERT/p' |
+          openssl x509 -text 2>/dev/null |
+          sed -n 's/ *Not After : *//p'`
+
+if [ -n "$end_date" ]
+then
+    end_date_seconds=`date '+%s' --date "$end_date"`
+    now_seconds=`date '+%s'`
+    echo "($end_date_seconds-$now_seconds)/24/3600" | bc
+fi
+;;
+
+-i)
+issue_dn=`openssl s_client -servername $host -connect $host:$port -showcerts -prexit </dev/null 2>/dev/null |
+          sed -n '/BEGIN CERTIFICATE/,/END CERT/p' |
+          openssl x509 -text 2>/dev/null |
+          sed -n 's/ *Issuer: *//p'`
+
+if [ -n "$issue_dn" ]
+then
+    issuer=`echo $issue_dn | sed -n 's/.*CN=*//p'`
+    echo $issuer
+fi
+;;
+*)
+echo "usage: $0 [-i|-d] hostname port"
+echo "    -i Show Issuer"
+echo "    -d Show valid days remaining"
+;;
+esac
